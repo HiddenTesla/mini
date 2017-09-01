0115baaac9cd7a225c47f65b5ca447c50e1e5d64
erwin <chen90071@163.com>
2017-09-01 23:45:43 +0800

    Initial commit

---
+++ helloworld.c
@@ -0,0 +1,36 @@
+#ifndef __cplusplus
+
+#define     _           main
+#define     __          {
+#define     ___         "Hello world"
+#define     ____        int
+#define     _____       0
+#define     ______      printf
+#define     _______     "\n"
+#define     ________    return
+#define     _________   }
+#define     __________  ;
+#define     ___________ (
+#define     ____________ )
+#define     _____________ void
+
+#define     ______________ \
+            ____ _ ___________ _____________ ____________\
+            __ ______ ___________ ___ _______ ____________ __________ \
+            ________ _____ __________ _________
+            
+            
+            
+#include <stdio.h>
+______________
+
+#else
+    
+#include <iostream>
+int main(void)
+{
+    std::cout << "Hello world\n";
+    return 0;
+}
+    
+#endif
